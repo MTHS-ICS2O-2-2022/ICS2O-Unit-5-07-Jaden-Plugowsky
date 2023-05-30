d29c50b95e76d1ade6c4617b7e991ddaa1cc326c
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-05-30 01:47:29 +0000

    final website

--- js/script.js
+++ js/script.js
@@ -4,17 +4,28 @@
 // Created on: May 2023
 // This file contains the JS functions for index.html
 
-"use strict";
+"use strict"
 
 function buttonOneClicked() {
-  //This function does XXXX
-  //Input through Textfields
-  const example = parseFloat(document.getElementById("example").value)
+  //This program takes a user-given number and adds every natural number from 1 to that number together, then giving you the sum
+  //Input through Textfield
+  const valueOfN = parseInt(document.getElementById("value-of-n").value)
+  let answer = null
 
   //Process
-  const answer = example
+  if (valueOfN >= 1 && valueOfN % 1 == 0) {
+    // Only allows positive, whole numbers above 0.
+    for (let counter = 1; counter <= valueOfN; counter++) {
+      answer += counter
+    }
+  } else {
+    document.getElementById("answer").innerHTML =
+      "That input is not a natural number; it is either negative, zero, a decimal, or simply not a number."
+  }
 
   //Output
-  document.getElementById("answer").innerHTML =
-    "The answer is: " + answer.toFixed(2)
+  if (valueOfN >= 1 && valueOfN % 1 == 0) {
+    document.getElementById("answer").innerHTML =
+      "The sum of all the numbers from 1 to " + valueOfN + " is: " + answer
+  }
 }
